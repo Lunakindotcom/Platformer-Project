33345b9516010fabf82ee2d4a106e094a7a9bfda
Lunakindotcom <alessia.locano.g53521@mcast.edu.mt>
2026-05-23 20:14:55 +0200

    Changed things for export, like resolution, canvas scaling, Tag and Shorting layers and also the size of the menu screens

--- Assets/Scripts/Lose.cs
+++ Assets/Scripts/Lose.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Lose : MonoBehaviour
 {
+    public GameObject enemyObject;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            Destroy(enemyObject);
             GameManager manager = FindAnyObjectByType<GameManager>();
             manager.Lost();
         }
--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,10 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Win : MonoBehaviour
 {
+    public GameObject playerObject;
+    public GameObject enemyObject;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            Destroy(playerObject);
+            Destroy(enemyObject);
             GameManager manager = FindAnyObjectByType<GameManager>();
             manager.Finish();
         }
